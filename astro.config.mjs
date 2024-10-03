faf4ddcc6d7f72a7c4d67833e0f4701f9e668aed
Jeffrey A <44594550+jeffrey-agyepong@users.noreply.github.com>
2024-10-03 14:49:55 -0600

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,42 @@
+import { defineConfig } from 'astro/config'
+import storyblok from '@storyblok/astro'
+import { loadEnv } from 'vite'
+import tailwind from '@astrojs/tailwind'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import vercel from '@astrojs/vercel/serverless'
+const env = loadEnv('', process.cwd(), 'STORYBLOK')
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
+      components: {
+        page: 'storyblok/Page',
+        feature: 'storyblok/Feature',
+        grid: 'storyblok/Grid',
+        teaser: 'storyblok/Teaser',
+        hero: 'storyblok/Hero',
+        config: 'storyblok/Config',
+        'popular-articles': 'storyblok/PopularArticles',
+        'all-articles': 'storyblok/AllArticles',
+        article: 'storyblok/Article',
+      },
+      apiOptions: {
+        region: "ca",
+      },
+    }),
+    tailwind(),
+  ],
+  output: env.STORYBLOK_IS_PREVIEW === 'yes' ? 'server' : 'hybrid',
+  ...(env.STORYBLOK_ENV === 'development' && {
+    vite: {
+      plugins: [basicSsl()],
+      server: {
+        https: false,
+      },
+    },
+  }),
+  adapter: vercel(),
+})
