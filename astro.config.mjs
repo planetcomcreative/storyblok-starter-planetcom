0479581b6da57a2081c89853d88c524542c70767
Jeffrey A <44594550+jeffrey-agyepong@users.noreply.github.com>
2024-10-08 11:11:45 -0600

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,43 +1,39 @@
-import { defineConfig } from 'astro/config'
-import storyblok from '@storyblok/astro'
-import { loadEnv } from 'vite'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import basicSsl from '@vitejs/plugin-basic-ssl'
-import vercel from '@astrojs/vercel/serverless'
-const env = loadEnv('', process.cwd(), 'STORYBLOK')
+const env = loadEnv("", process.cwd(), "STORYBLOK");
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
-      bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
+      bridge: env.STORYBLOK_IS_PREVIEW === "yes",
       components: {
-        page: 'storyblok/Page',
-        feature: 'storyblok/Feature',
-        grid: 'storyblok/Grid',
-        teaser: 'storyblok/Teaser',
-        contactInfo:'storyblok/ContactInfo',
-        hero: 'storyblok/Hero',
-        config: 'storyblok/Config',
-        'popular-articles': 'storyblok/PopularArticles',
-        'all-articles': 'storyblok/AllArticles',
-        article: 'storyblok/Article',
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+        contactInfo: "storyblok/ContactInfo",
+        hero: "storyblok/Hero",
+        config: "storyblok/Config",
+        "popular-articles": "storyblok/PopularArticles",
+        "all-articles": "storyblok/AllArticles",
+        article: "storyblok/Article",
       },
       apiOptions: {
         region: "ca",
       },
     }),
-    tailwind(),
-  ],
-  output: env.STORYBLOK_IS_PREVIEW === 'yes' ? 'server' : 'hybrid',
-  ...(env.STORYBLOK_ENV === 'development' && {
+    tailwind(),],
     vite: {
       plugins: [basicSsl()],
       server: {
-        https: false,
+        https: true,
       },
     },
-  }),
-  adapter: vercel(),
-})
+
+});
